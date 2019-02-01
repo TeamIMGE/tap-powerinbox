90b8ad9cc88f5b5c73c73ad11a992d56f9048c3f
Aditya Khemchandani <khemchandaniaditya@gmail.com>
2019-02-01 12:41:53 -0500

    adding ext_req and pin dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 from setuptools import setup
 
 setup(
@@ -10,9 +9,14 @@ setup(
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_powerinbox"],
     install_requires=[
-        "singer-python==5.0.12",
-        "requests",
+        "singer-python==5.4.1",
+        "requests==2.13.0",
+        'backoff==1.3.2',
     ],
+    extras_require=[
+        "ipdb==0.11",
+        "pylint==1.9.4"
+    ]
     entry_points="""
     [console_scripts]
     tap-powerinbox=tap_powerinbox:main
