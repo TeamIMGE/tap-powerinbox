763575dae8bc182d902286d87b7c83060315e126
Aditya Khemchandani <khemchandaniaditya@gmail.com>
2019-02-01 12:33:13 -0500

    updating do sync function to use state value

--- tap_powerinbox/__init__.py
+++ tap_powerinbox/__init__.py
@@ -13,11 +13,11 @@ from singer import utils, metrics
 
 
 REQUIRED_CONFIG_KEYS = ["guid", "company_id", "start_date"]
-PER_PAGE = 100
 BASE_URL = "https://reports.revenuestripe.com/company/"
-DATE_FORMAT = '%Y-%m-%d'
+DATE_FORMAT = "%Y-%m-%d"
 
 CONFIG = {}
+STATE = {}
 
 LOGGER = singer.get_logger()
 SESSION = requests.session()
@@ -30,11 +30,18 @@ def load_schema():
     """get the schema"""
     return utils.load_json(get_abs_path("schemas/schema.json"))
 
+def get_start_date(key):
+    """get the start_date form state"""
+    if key not in STATE:
+        STATE[key] = CONFIG["start_date"]
+
+    return STATE[key]
+
 def on_giveup(details):
     """giveup logic for backoff"""
     url = details["args"]
     raise Exception("Giving up on request after {} tries with url {} " \
-                    .format(details['tries'], url))
+                    .format(details["tries"], url))
 
 """Calls to the API is limited to a maximum of 3 tries if a request isn't completed successfully.
 The tap should remain well below this max during normal use. """
@@ -66,7 +73,12 @@ def do_sync(guid, company_id, start_date):
     singer.write_schema("powerinbox_response", schema, "stripe")
 
     LOGGER.info("---------- Starting sync ----------")
-    next_date = start_date
+
+    pickup_date = get_start_date("start_date")
+    if (pickup_date):
+        next_date = pickup_date
+    else:
+        next_date = start_date
 
     try:
         while next_date < utils.strftime(utils.now(), DATE_FORMAT):
@@ -78,25 +90,27 @@ def do_sync(guid, company_id, start_date):
 
             for record in response:
                 singer.write_records("powerinbox_response", [record])
-                state = {"start_date": next_date.encode('ascii', 'ignore')}
+                utils.update_state(STATE, "start_date", next_date.encode("ascii", "ignore"))
 
-            singer.write_state(state)
             next_date = utils.strftime((utils.strptime_to_utc(next_date)+
                                         timedelta(days=1)), DATE_FORMAT)
 
     except Exception as exc:
         LOGGER.critical(exc)
+        singer.write_state(STATE)
         raise exc
 
+    singer.write_state(STATE)
     LOGGER.info("---------- Completed sync ----------")
 
 def main():
     """main function"""
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
     CONFIG.update(args.config)
+    STATE.update(args.state)
     guid = CONFIG["guid"]
     company_id = CONFIG["company_id"]
-    start_date = CONFIG.get('start_date')
+    start_date = CONFIG.get("start_date")
     do_sync(guid, company_id, start_date)
 
 if __name__ == "__main__":
